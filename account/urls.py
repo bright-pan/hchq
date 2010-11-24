e71c8c73f250569aea9d2aba50b0234cc4716d8b
Bright Pan <loststriker@gmail.com>
2010-11-24 16:47:33 +0800

    Fix url error and template error

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('hchq.account.views',
                         
-      (r'^account/login$', 'login', {'template_name' : 'account/login.html'}),
+#      (r'login$', 'login', {'template_name' : 'account/login.html'}),
 #      (r'^account/logout$', logout, {'template_name' : 'account/logout.html'}),
 #      (r'^account/login$', login, {'template_name' : 'account/login.html'}),
 #      (r'^account/login$', login, {'template_name' : 'account/login.html'}),
+                        
+)
+
+urlpatterns += patterns('django.contrib.auth.views',
+                        
+      (r'login$', 'login', {'template_name' : 'account/login.html'}),
                         
 )
--- urls.py
+++ urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('hchq.account.views',
 
 urlpatterns += patterns('',
 
-      (r'^account/', include('hchq.account.views.urls')),
+      (r'^account/', include('hchq.account.urls')),
 #      (r'^account/logout$', logout, {'template_name' : 'account/logout.html'}),
 #      (r'^account/login$', login, {'template_name' : 'account/login.html'}),
 #      (r'^account/login$', login, {'template_name' : 'account/login.html'}),
