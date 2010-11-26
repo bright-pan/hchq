5c2468106c274423039f015686249e6866335b7d
Bright Pan <loststriker@gmail.com>
2010-11-26 11:30:48 +0800

    Fix login_form

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 class login_form(forms.Form):
+    """
+    用户登入表单
+    """
     username_error_messages={'required': _(u'请输入用户名称！'),
                              'max_length': _(u'输入的用户名称长度大于10个汉字！'),
                              'do_not_exist': _(u'你所输入的用户不存在！'),
@@ -26,30 +29,36 @@ class login_form(forms.Form):
         max_length=30,
         required=True, 
         label=_(u'用户密码'), 
-        widget=forms.TextInput(attrs={'class':'',
+        widget=forms.PasswordInput(attrs={'class':'',
                                       'size':'30',}), 
         help_text=_(u'例如：123456'),
         error_messages = password_error_messages,
     )
     
     def clean_username(self):
-
         try:
             username = self.data.get('username')
             User.objects.get(username=username)
         except ObjectDoesNotExist:
             raise forms.ValidationError(self.username_error_messages['do_not_exist'])
         return username
+
     def clean_password(self):
 
         try:
             username = self.data.get('username')
             password = self.data.get('password')
             from django.contrib.auth import authenticate
-            user = authenticate(username = username, password = password)
-            if user is None:
+            self.user = authenticate(username = username, password = password)
+            if self.user is None:
                 raise forms.ValidationError(self.password_error_messages['password_error'])
         except ObjectDoesNotExist:
             raise forms.ValidationError(self.username_error_messages['do_not_exist'])
         return password
+    
+    def get_user(self):
+        return self.user
+    
+    def get_user_id(self):
+        return self.user.id
         
