bab26663e1ee7e0711a35863a7bfa003b026ca91
Bright Pan <loststriker@gmail.com>
2010-12-02 14:38:05 +0800

    Fix service area models

--- service_area/models.py
+++ service_area/models.py
@@ -1,12 +1,23 @@
 #coding=utf-8
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 # Create your models here.
 
 class ServiceArea(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=64, unique=True)
     creater = models.ForeignKey(User)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = 'service_area'
+        ordering = ['-created_at']
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('service_area_show', (), {'service_area_id': self.id})
