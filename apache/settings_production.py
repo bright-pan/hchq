b56f5d260bb235ae510b89b801e2615ee65cc7b5
Bright Pan <loststriker@gmail.com>
2011-01-24 10:51:06 +0800

    Fix production settings

--- apache/settings_production.py
+++ apache/settings_production.py
@@ -5,3 +5,4 @@ from hchq.settings import *
 DEBUG = TEMPLATE_DEBUG = False
 ROOT_URLCONF = 'hchq.apache.urls_production'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True                 # Whether a user's session cookie expires when the Web browser is closed.
