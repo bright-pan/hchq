5187f7cf8cd4f23283a44d1d5121a59f9afc155b
Bright Pan <loststriker@gmail.com>
2011-10-10 10:36:50 +0800

    modify mysql password to 2011

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ DATABASES = {
         'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'hchq',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '1234',                  # Not used with sqlite3.
+        'PASSWORD': '2011',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
