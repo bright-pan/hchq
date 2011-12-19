2fdfdd71ed67e2583657b4f1bd682a782533120f
bright-pan <loststriker@gmail.com>
2011-12-19 12:08:14 +0800

    fix update process and if the source code is not need to update, the apache demo is not been restart.

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 cd e:/work/hchq
 pwd
-git fetch
-git merge origin/develop
-git status
-net stop Apache2.2
-net start Apache2.2
+status=$(git fetch)
+if [ -z $status];then
+    echo u'the source code is clean and don`t update!!!'
+else
+    echo u'the source code need to update!!!'
+    git merge origin/develop
+    echo 'the source code has updated!!!'
+    git status
+    echo u'restart apache demo!!!'
+    net stop Apache2.2
+    net start Apache2.2
+fi
+
