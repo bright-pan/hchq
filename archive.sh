4b866e478c71733bdc0732eff34c15aff7c114ab
Bright Pan <loststriker@gmail.com>
2011-12-21 11:27:58 +0800

    Fix archive method

--- archive.sh
+++ archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "****************************************"
-echo "Archiving for anti-stealing $1"
+echo "Archiving for hchq $1"
 git archive --format=tar --prefix=hchq/ $1 | (cd .. && gzip > hchq-$1.tar.gz)
-echo "Complete"
+echo "Complete archive hchq $1"
 echo "****************************************"
