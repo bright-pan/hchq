9206277456cb01b9b4964bb99ba393ed22762a1d
Bright Pan <loststriker@gmail.com>
2012-04-10 09:29:32 +0800

    Delete remote update function and fix backup function

--- mysql_backup.bat
+++ mysql_backup.bat
@@ -1,14 +1,8 @@
 @echo off
-:: 设置今天的日期
 set today=%date:~0,4%%date:~5,2%%date:~8,2%
-:: 用来备份的文件名
 set sql=hchq_%today%.sql
-:: 备份文件存储位置
 set backup_path=E:\work\mysql_backup
-:: mysql bin目录
 set mysql_bin=C:\Program Files\MySQL\MySQL Server 5.1\bin
-
-:: 开始数据备份，请修改-uroot中的root（数据库用户名）和-pezool_org中的ezool_org(数据库密码)
 C:
 cd %mysql_bin%
 echo backup %sql%...
--- update.sh
+++ update.sh
@@ -1,20 +1,3 @@
 cd e:/work/hchq
 pwd
-git fetch
-status=$(git status)
-s=$(expr "$status" : ".*fast-forwarded.")
-t="0"
-echo $s
-if [ "$s" = "$t" ];
-then
-    echo 'the source code is clean and do not update!!!'
-else
-    echo "the source code need to update!!!"
-    git merge origin/develop
-    echo "the source code has updated!!!"
-    git status
-    echo "restart apache demo!!!"
-    net stop Apache2.2
-    net start Apache2.2
-fi
 cmd /c mysql_backup.bat
