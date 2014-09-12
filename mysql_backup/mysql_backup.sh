6b385e95158bff39307eba5fd59b9b97e92def3d
Bright Pan <bright_pan@163.com>
2014-09-12 09:54:13 +0800

    Fix mysql backup

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -6,4 +6,8 @@ mysqldump -uroot -phchq2010-2014*.* hchq | gzip > hchq-$filename.sql.gz
 cd ../static/images/
 tar czf ../../mysql_backup/photos.tar.gz photos
 tar czf ../../mysql_backup/thumbnails.tar.gz thumbnails
-
+cd ../../mysql_backup/
+gpg -a --output hchq-$filename.sql.gz.rsa -r bright_pan@163.com -e hchq-$filename.sql.gz
+gpg -a --output photos.tar.gz.rsa -r bright_pan@163.com -e photos.tar.gz
+gpg -a --output thumbnails.tar.gz.rsa -r bright_pan@163.com -e thumbnails.tar.gz
+rm -rf hchq-$filename.sql.gz photos.tar.gz thumbnails.tar.gz mysql_backup.sh
