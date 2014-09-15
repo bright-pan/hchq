e2bfc9b7d02e975bf872e4467ca10e955537fa02
Bright Pan <bright_pan@163.com>
2014-09-15 17:12:33 +0800

    Fix backup for using git

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -7,7 +7,7 @@ cd ../static/images/
 tar czf ../../mysql_backup/photos.tar.gz photos
 tar czf ../../mysql_backup/thumbnails.tar.gz thumbnails
 cd ../../mysql_backup/
-gpg -a --output hchq-$filename.sql.gz.rsa -r bright_pan@163.com -e hchq-$filename.sql.gz
-gpg -a --output photos.tar.gz.rsa -r bright_pan@163.com -e photos.tar.gz
-gpg -a --output thumbnails.tar.gz.rsa -r bright_pan@163.com -e thumbnails.tar.gz
-rm -rf hchq-$filename.sql.gz photos.tar.gz thumbnails.tar.gz mysql_backup.sh
+#gpg -a --output hchq-$filename.sql.gz.rsa -r bright_pan@163.com -e hchq-$filename.sql.gz
+#gpg -a --output photos.tar.gz.rsa -r bright_pan@163.com -e photos.tar.gz
+#gpg -a --output thumbnails.tar.gz.rsa -r bright_pan@163.com -e thumbnails.tar.gz
+#rm -rf hchq-$filename.sql.gz photos.tar.gz thumbnails.tar.gz mysql_backup.sh
