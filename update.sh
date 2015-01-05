e0e32987a4b839f9aad6d0ba9d40266a18364788
BrightPan <bright_pan@163.com>
2015-01-05 13:45:17 +0800

    fix update script

--- update.sh
+++ update.sh
@@ -1,15 +1,12 @@
-#!/bin/bash                                                                                                                                                           
+#!/bin/bash
 cd
-rm -rf hchq_bk hchq.zip
+rm -rf hchq_bk
 mv hchq hchq_bk
-curl -L -o hchq.zip https://codeload.github.com/bright-pan/hchq/zip/master
-unzip hchq.zip
-mv hchq-master hchq
-cp -rf hchq_bk/static/images/photos hchq/static/images/photos
-cp -rf hchq_bk/static/images/thumbnails hchq/static/images/thumbnails
+git clone https://github.com/bright-pan/hchq
+cp -rf hchq_bk/static/images/photos hchq/static/images/
+cp -rf hchq_bk/static/images/thumbnails hchq/static/images/
 cd hchq/mysql_backup
 ./mysql_backup.sh
-
 cd ~/hchq
 mv mysql_backup ../
 ./dist.sh
